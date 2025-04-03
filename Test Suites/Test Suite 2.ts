7c56184ebca7055121bd61d8b3732a4866a055ae
phat.tcu@katalon.com <katalon_g3_dummy_username>
2025-04-03 02:31:05 +0000

    Delete "TC_3819_UserRegistration_EmailVerification_10" test case

--- Test Suites/Test Suite 2.ts
+++ Test Suites/Test Suite 2.ts
@@ -1,61 +1,39 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
-   <description></description>
-   <name>Test Suite 2</name>
-   <tag></tag>
-   <isRerun>false</isRerun>
-   <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
-   <pageLoadTimeout>30</pageLoadTimeout>
-   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
-   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
-   <testSuiteGuid>1ed41388-4652-4c22-ae88-3bf408ae9f24</testSuiteGuid>
-   <testCaseLink>
-      <guid>5c5a694b-9542-47df-903f-8f85631ec816</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Main Test Cases/TC_7821_ProfileUpdate_UI_06</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>425fcb46-baf4-4c23-a69a-75a65b095b3e</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Main Test Cases/TC_2374_PasswordReset_Security_07</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>d0b7df57-90f5-4e37-b147-22bb14d5a1b9</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Main Test Cases/TC_5048_AdminDashboard_Statistics_08</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>dc203119-4ac8-4773-af55-7f85f9b025ef</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Main Test Cases/TC_6792_ProductSearch_Filter_09</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>7098cf6c-7973-42fa-8b83-4259563a7bcb</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Main Test Cases/TC_3819_UserRegistration_EmailVerification_10</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>be9d1a8d-ddde-48c8-90cf-fd0876391cef</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>8ec25d6a-451e-4894-9fe7-d3949be2c853</variableId>
-      </variableLink>
-   </testCaseLink>
+    <name>Test Suite 2</name>
+    <description></description>
+    <tag></tag>
+    <isRerun>false</isRerun>
+    <mailRecipient></mailRecipient>
+    <numberOfRerun>0</numberOfRerun>
+    <pageLoadTimeout>30</pageLoadTimeout>
+    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+    <rerunImmediately>false</rerunImmediately>
+    <testSuiteGuid>1ed41388-4652-4c22-ae88-3bf408ae9f24</testSuiteGuid>
+    <testCaseLink>
+        <guid>5c5a694b-9542-47df-903f-8f85631ec816</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Main Test Cases/TC_7821_ProfileUpdate_UI_06</testCaseId>
+    </testCaseLink>
+    <testCaseLink>
+        <guid>425fcb46-baf4-4c23-a69a-75a65b095b3e</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Main Test Cases/TC_2374_PasswordReset_Security_07</testCaseId>
+    </testCaseLink>
+    <testCaseLink>
+        <guid>d0b7df57-90f5-4e37-b147-22bb14d5a1b9</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Main Test Cases/TC_5048_AdminDashboard_Statistics_08</testCaseId>
+    </testCaseLink>
+    <testCaseLink>
+        <guid>dc203119-4ac8-4773-af55-7f85f9b025ef</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Main Test Cases/TC_6792_ProductSearch_Filter_09</testCaseId>
+    </testCaseLink>
 </TestSuiteEntity>
+
