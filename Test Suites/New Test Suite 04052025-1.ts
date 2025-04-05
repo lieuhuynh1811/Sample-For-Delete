dd918e0f1f0b0db695d1d0d69b560068cec59472
Phat Cu <phat.tcu@katalon.com>
2025-04-05 16:58:48 +0700

    Add new test cases into test suite

--- Test Suites/New Test Suite 04052025-1.ts
+++ Test Suites/New Test Suite 04052025-1.ts
@@ -11,4 +11,25 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>1745f881-cca6-4b58-80fd-1ae2aa9c14e1</testSuiteGuid>
+   <testCaseLink>
+      <guid>6e8b54b8-bcb1-454b-9080-45355683a781</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Test Cases/TC_2374_PasswordReset_Security_07</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8076596d-1528-4620-b8a2-57767c0972c3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Test Cases/TC_5048_AdminDashboard_Statistics_08</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>79665f24-38ca-49fc-b21e-2432a66ab347</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Test Cases/TC_6792_ProductSearch_Filter_09</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
