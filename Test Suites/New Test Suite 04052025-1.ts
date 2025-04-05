d9bd8be9b5582855c82b7ded9c30bf73a802c126
Phat Cu <phat.tcu@katalon.com>
2025-04-05 16:57:09 +0700

    Add new test suite

---
+++ Test Suites/New Test Suite 04052025-1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>New test suite 04052025-01</description>
+   <name>New Test Suite 04052025-1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1745f881-cca6-4b58-80fd-1ae2aa9c14e1</testSuiteGuid>
+</TestSuiteEntity>
