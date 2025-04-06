546bbd098f1d3942b7a23ce16d5f1432c3003f57
phat.tcu@katalon.com <katalon_g3_dummy_username>
2025-04-06 15:03:01 +0700

    Rename test suite

---
+++ Test Suites/New test suite 1 (renamed).ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+   <name>New test suite 1 (renamed)</name>
+   <description></description>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>4eba6a10-9017-4faf-a4e7-0a1c45f2e371</testSuiteGuid>
+</TestSuiteEntity>
