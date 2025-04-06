4a2074205b727564cd0d122532d69bfb6fca581e
phat.tcu@katalon.com <katalon_g3_dummy_username>
2025-04-06 13:12:34 +0700

    Create "New test suite 3" test suite

---
+++ Test Suites/New test suite 3.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>New test suite 3</name>
+    <description></description>
+    <tag></tag>
+    <isRerun>false</isRerun>
+    <mailRecipient></mailRecipient>
+    <numberOfRerun>0</numberOfRerun>
+    <pageLoadTimeout>30</pageLoadTimeout>
+    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+    <rerunImmediately>false</rerunImmediately>
+    <testSuiteGuid>aec3eda7-70e0-4e20-9518-34649eaa4b55</testSuiteGuid>
+    <testCaseLink>
+        <guid>4ed4d945-d7f3-4ef1-a7de-3e5865c216fd</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Common Test Cases/Login</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
+        <variableLink>
+            <testDataLinkId></testDataLinkId>
+            <type>DEFAULT</type>
+            <value></value>
+            <variableId>19fe967b-1564-4705-aff2-848d45b84489</variableId>
+        </variableLink>
+        <variableLink>
+            <testDataLinkId></testDataLinkId>
+            <type>DEFAULT</type>
+            <value></value>
+            <variableId>75fa3d79-7e92-4d55-b359-f7c9493ea288</variableId>
+        </variableLink>
+    </testCaseLink>
+    <testCaseLink>
+        <guid>3da2074a-7819-4b0a-8307-bda6423c109c</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Main Test Cases/TC_7821_ProfileUpdate_UI_06</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
+        <variableLink>
+            <testDataLinkId></testDataLinkId>
+            <type>DEFAULT</type>
+            <value></value>
+            <variableId></variableId>
+        </variableLink>
+    </testCaseLink>
+</TestSuiteEntity>
+
