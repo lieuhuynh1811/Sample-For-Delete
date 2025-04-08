1840495c3d1b18a65e0163f5dcea726c5a057581
phat.tcu@katalon.com <katalon_g3_dummy_username>
2025-04-08 03:47:49 +0000

    Delete "New folder after deployment (1)" test folder

--- Test Suites/Test Suite 1.ts
+++ Test Suites/Test Suite 1.ts
@@ -1,15 +1,14 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
-    <name>Test Suite 1</name>
-    <description></description>
-    <tag></tag>
-    <isRerun>false</isRerun>
-    <mailRecipient></mailRecipient>
-    <numberOfRerun>0</numberOfRerun>
-    <pageLoadTimeout>30</pageLoadTimeout>
-    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
-    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-    <rerunImmediately>false</rerunImmediately>
-    <testSuiteGuid>01648cce-8f35-4743-9a98-fcae96146c22</testSuiteGuid>
+   <name>Test Suite 1</name>
+   <description></description>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>01648cce-8f35-4743-9a98-fcae96146c22</testSuiteGuid>
 </TestSuiteEntity>
-
--- Test Suites/Test Suite 2.ts
+++ Test Suites/Test Suite 2.ts
@@ -1,33 +1,32 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
-    <name>Test Suite 2</name>
-    <description></description>
-    <tag></tag>
-    <isRerun>false</isRerun>
-    <mailRecipient></mailRecipient>
-    <numberOfRerun>0</numberOfRerun>
-    <pageLoadTimeout>30</pageLoadTimeout>
-    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
-    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-    <rerunImmediately>false</rerunImmediately>
-    <testSuiteGuid>1ed41388-4652-4c22-ae88-3bf408ae9f24</testSuiteGuid>
-    <testCaseLink>
-        <guid>5c5a694b-9542-47df-903f-8f85631ec816</guid>
-        <isReuseDriver>false</isReuseDriver>
-        <isRun>true</isRun>
-        <testCaseId>Test Cases/Main Test Cases/TC_7821_ProfileUpdate_UI_06</testCaseId>
-    </testCaseLink>
-    <testCaseLink>
-        <guid>425fcb46-baf4-4c23-a69a-75a65b095b3e</guid>
-        <isReuseDriver>false</isReuseDriver>
-        <isRun>true</isRun>
-        <testCaseId>Test Cases/Main Test Cases/TC_2374_PasswordReset_Security_07</testCaseId>
-    </testCaseLink>
-    <testCaseLink>
-        <guid>dc203119-4ac8-4773-af55-7f85f9b025ef</guid>
-        <isReuseDriver>false</isReuseDriver>
-        <isRun>true</isRun>
-        <testCaseId>Test Cases/Main Test Cases/TC_6792_ProductSearch_Filter_09</testCaseId>
-    </testCaseLink>
+   <name>Test Suite 2</name>
+   <description></description>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1ed41388-4652-4c22-ae88-3bf408ae9f24</testSuiteGuid>
+   <testCaseLink>
+      <guid>5c5a694b-9542-47df-903f-8f85631ec816</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Test Cases/TC_7821_ProfileUpdate_UI_06</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>425fcb46-baf4-4c23-a69a-75a65b095b3e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Test Cases/TC_2374_PasswordReset_Security_07</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dc203119-4ac8-4773-af55-7f85f9b025ef</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Test Cases/TC_6792_ProductSearch_Filter_09</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
-
