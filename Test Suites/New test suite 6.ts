749ebf0e1684bcc3ff4e1dfcb8c44075ebea7708
phat.tcu@katalon.com <katalon_g3_dummy_username>
2025-04-11 00:21:37 +0700

    Create "New test suite 6" test suite

---
+++ Test Suites/New test suite 6.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>New test suite 6</name>
+    <description></description>
+    <tag></tag>
+    <isRerun>false</isRerun>
+    <mailRecipient></mailRecipient>
+    <numberOfRerun>0</numberOfRerun>
+    <pageLoadTimeout>30</pageLoadTimeout>
+    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+    <rerunImmediately>false</rerunImmediately>
+    <testSuiteGuid>1bbbd89e-f4bf-4a2e-864e-4901a348ea2c</testSuiteGuid>
+    <testCaseLink>
+        <guid>3ef50b82-10fd-4e22-b4e8-5f72f651db14</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Main Test Cases/TC_7821_ProfileUpdate_UI_06</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+    </testCaseLink>
+</TestSuiteEntity>
+
