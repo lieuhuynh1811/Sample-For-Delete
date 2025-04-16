885056dead91f0a737d5c43cf95cc003e0439d2b
phat.tcu@katalon.com <katalon_g3_dummy_username>
2025-04-16 11:26:21 +0700

    Create "New test suite 7" test suite

--- Test Suites/New test suite 7.ts
+++ Test Suites/New test suite 7.ts
@@ -10,16 +10,16 @@
     <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
     <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
     <rerunImmediately>true</rerunImmediately>
-    <testSuiteGuid>91755fb9-428c-436a-9d42-94ed3e57585d</testSuiteGuid>
+    <testSuiteGuid>cb700131-22b2-4794-a198-c7f5815c06d5</testSuiteGuid>
     <testCaseLink>
-        <guid>3ff397ac-cc96-4582-b2b4-b594d4e0f62c</guid>
+        <guid>33c4c988-5dd3-4180-8544-05d740efe5ce</guid>
         <isReuseDriver>false</isReuseDriver>
         <isRun>true</isRun>
         <testCaseId>Test Cases/Main Test Cases/TC_7821_ProfileUpdate_UI_06</testCaseId>
         <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
     </testCaseLink>
     <testCaseLink>
-        <guid>ae670fdb-3a69-403c-b90d-327f05b58c4d</guid>
+        <guid>3bf29e11-dddc-49f8-bcbc-31852b6b10bf</guid>
         <isReuseDriver>false</isReuseDriver>
         <isRun>true</isRun>
         <testCaseId>Test Cases/Main Test Cases/TC_7821_ProfileUpdate_UI_06</testCaseId>
