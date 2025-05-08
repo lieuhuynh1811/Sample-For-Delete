c5ce7446307e88d6b09137f6117910ea951cc60b
phat.tcu@katalon.com <katalon_g3_dummy_username>
2025-05-08 17:48:57 +0700

    Create "New Test Suites 05082025-01" test suite

---
+++ Test Suites/New Test Suites 05082025-01.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+    <name>New Test Suites 05082025-01</name>
+    <description></description>
+    <tag></tag>
+    <isRerun>false</isRerun>
+    <mailRecipient></mailRecipient>
+    <numberOfRerun>3</numberOfRerun>
+    <pageLoadTimeout>30</pageLoadTimeout>
+    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+    <rerunImmediately>true</rerunImmediately>
+    <testSuiteGuid>290fe83b-3364-4a97-97ef-3ba0e9736462</testSuiteGuid>
+</TestSuiteEntity>
