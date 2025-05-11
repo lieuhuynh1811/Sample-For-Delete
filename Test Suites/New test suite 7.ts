b3feb8417afec6a8f14d2f14478331b7cf31c945
phat.tcu@katalon.com <katalon_g3_dummy_username>
2025-05-11 15:49:08 +0700

    TO commit new changes

--- Test Suites/New test suite 7.ts
+++ Test Suites/New test suite 7.ts
@@ -1,29 +1,28 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
-    <name>New test suite 7</name>
-    <description></description>
-    <tag></tag>
-    <isRerun>false</isRerun>
-    <mailRecipient></mailRecipient>
-    <numberOfRerun>3</numberOfRerun>
-    <pageLoadTimeout>30</pageLoadTimeout>
-    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
-    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-    <rerunImmediately>true</rerunImmediately>
-    <testSuiteGuid>cb700131-22b2-4794-a198-c7f5815c06d5</testSuiteGuid>
-    <testCaseLink>
-        <guid>33c4c988-5dd3-4180-8544-05d740efe5ce</guid>
-        <isReuseDriver>false</isReuseDriver>
-        <isRun>true</isRun>
-        <testCaseId>Test Cases/Common Test Cases (updated) 1/Main Test Cases/TC_7821_ProfileUpdate_UI_06</testCaseId>
-        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-    </testCaseLink>
-    <testCaseLink>
-        <guid>3bf29e11-dddc-49f8-bcbc-31852b6b10bf</guid>
-        <isReuseDriver>false</isReuseDriver>
-        <isRun>true</isRun>
-        <testCaseId>Test Cases/Common Test Cases (updated) 1/Main Test Cases/TC_7821_ProfileUpdate_UI_06</testCaseId>
-        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-    </testCaseLink>
+   <name>New test suite 7</name>
+   <description></description>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>cb700131-22b2-4794-a198-c7f5815c06d5</testSuiteGuid>
+   <testCaseLink>
+      <guid>33c4c988-5dd3-4180-8544-05d740efe5ce</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Common Test Cases (updated) 1/Main Test Cases/TC_7821_ProfileUpdate_UI_06</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3bf29e11-dddc-49f8-bcbc-31852b6b10bf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Common Test Cases (updated) 1/Main Test Cases/TC_7821_ProfileUpdate_UI_06</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
-
