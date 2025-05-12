238ed638ed205a1487510097d83e6fe3e7d30a46
phat.tcu@katalon.com <katalon_g3_dummy_username>
2025-05-12 13:39:06 +0700

    TO commit new changes

--- Test Suites/New test suite 7.ts
+++ Test Suites/New test suite 7.ts
@@ -13,32 +13,6 @@
    <testSuiteGuid>cb700131-22b2-4794-a198-c7f5815c06d5</testSuiteGuid>
    <testCaseLink></testCaseLink>
    <testCaseLink>
-      <guid>b1463eab-f5da-49a9-81ea-fc66a0b1a19e</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Common Test Cases (updated) 1/Main Test Cases/TC_3819_UserRegistration_EmailVerification_10 (updated)</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-      <variableLinks>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>be9d1a8d-ddde-48c8-90cf-fd0876391cef</variableId>
-      </variableLinks>
-      <variableLinks>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>8ec25d6a-451e-4894-9fe7-d3949be2c853</variableId>
-      </variableLinks>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>f97f038c-468f-436c-be27-1134a47378d9</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Common Test Cases (updated) 1/Main Test Cases/TC_2837_ShoppingCart_Checkout_02 (updated)</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
       <guid>e7f75f71-9a5c-403b-b9c1-994d8ee0e059</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
