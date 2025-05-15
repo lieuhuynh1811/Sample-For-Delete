65c764f8f93ed4c2cc27ce3745e026d5c53b5aaf
phat.tcu@katalon.com <katalon_g3_dummy_username>
2025-05-15 12:15:57 +0700

    TO commit new changes

--- Test Suites/New test suite 7.ts
+++ Test Suites/New test suite 7.ts
@@ -31,7 +31,21 @@
       </variableLinks>
    </testCaseLink>
    <testCaseLink>
-      <guid>e86baeeb-04b8-415b-ade5-3c39ca0d166b</guid>
+      <guid>483482e8-48fd-4e42-9483-b3fb8bf9a47d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Common Test Cases (updated) 1/Main Test Cases/TC_5048_AdminDashboard_Statistics_08</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e61e12c8-c4cd-4f92-9969-bdd40bf4bc7f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Common Test Cases (updated) 1/Main Test Cases/TC_6792_ProductSearch_Filter_09</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>71db3895-d07f-4cdb-bcf4-8fea57190433</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Common Test Cases (updated) 1/Main Test Cases/TC_3819_UserRegistration_EmailVerification_10 (updated)</testCaseId>
@@ -50,7 +64,7 @@
       </variableLinks>
    </testCaseLink>
    <testCaseLink>
-      <guid>63177724-b668-4542-90dc-dc43a9824683</guid>
+      <guid>75c52821-7b66-4212-846e-ab8191639eda</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Common Test Cases (updated) 1/Main Test Cases/TC_9921_DataExport_CSV_Validation_04 (updated)</testCaseId>
