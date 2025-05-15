5627016bee79064b1035e18a8af5b8d913411c74
phat.tcu@katalon.com <katalon_g3_dummy_username>
2025-05-15 12:14:42 +0700

    TO commit new changes

--- Test Suites/New test suite 7.ts
+++ Test Suites/New test suite 7.ts
@@ -12,25 +12,6 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>cb700131-22b2-4794-a198-c7f5815c06d5</testSuiteGuid>
    <testCaseLink>
-      <guid>5b41fd88-2c26-402a-9adc-d6645c9846e0</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Common Test Cases (updated) 1/Main Test Cases/TC_3819_UserRegistration_EmailVerification_10 (updated)</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-      <variableLinks>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>be9d1a8d-ddde-48c8-90cf-fd0876391cef</variableId>
-      </variableLinks>
-      <variableLinks>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>8ec25d6a-451e-4894-9fe7-d3949be2c853</variableId>
-      </variableLinks>
-   </testCaseLink>
-   <testCaseLink>
       <guid>a766e9cd-371b-4191-aa63-bc3f7f1af3b3</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
@@ -47,6 +28,25 @@
          <type>DEFAULT</type>
          <value></value>
          <variableId>a97344e7-5f9c-4936-92a2-d85e5463f3c9</variableId>
+      </variableLinks>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e86baeeb-04b8-415b-ade5-3c39ca0d166b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Common Test Cases (updated) 1/Main Test Cases/TC_3819_UserRegistration_EmailVerification_10 (updated)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLinks>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>be9d1a8d-ddde-48c8-90cf-fd0876391cef</variableId>
+      </variableLinks>
+      <variableLinks>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>8ec25d6a-451e-4894-9fe7-d3949be2c853</variableId>
       </variableLinks>
    </testCaseLink>
 </TestSuiteEntity>
