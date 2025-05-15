d68cb5d24ca490acd117cf153c5e37dbf29f16dd
phat.tcu@katalon.com <katalon_g3_dummy_username>
2025-05-15 14:58:35 +0700

    Delete "TC_3819_UserRegistration_EmailVerification_10 (updated)" test case

--- Test Suites/New test suite 7.ts
+++ Test Suites/New test suite 7.ts
@@ -19,25 +19,6 @@
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>d3720512-5d6e-47b9-ab94-07525230be4a</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Common Test Cases (updated) 1/Main Test Cases/TC_3819_UserRegistration_EmailVerification_10 (updated)</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-      <variableLinks>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>be9d1a8d-ddde-48c8-90cf-fd0876391cef</variableId>
-      </variableLinks>
-      <variableLinks>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>8ec25d6a-451e-4894-9fe7-d3949be2c853</variableId>
-      </variableLinks>
-   </testCaseLink>
-   <testCaseLink>
       <guid>9a280604-82c6-49d9-a08e-66ba61e2b9d7</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>false</isRun>
