605888e2f81d64f9c08a03fa8f9bef6bebd8e502
phat.tcu@katalon.com <katalon_g3_dummy_username>
2025-05-19 11:09:07 +0700

    Duplicate "New folder 1" test folder

---
+++ Test Suites/New folder 1 (1)/New folder 1/New Test Suites 04222025-01 (updated) (1).ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <name>New Test Suites 04222025-01 (updated) (1)</name>
+   <description></description>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>65b75e88-7432-4303-a460-789a4fc5280b</testSuiteGuid>
+   <testCaseLink>
+      <guid>6289503a-57ef-4fe4-bb25-3ea30c45789d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Common Test Cases (updated) 1/Main Test Cases/TC_7821_ProfileUpdate_UI_06</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8d776317-95fb-4f4c-af35-169b838a91d4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Common Test Cases (updated) 1/Main Test Cases/TC_7821_ProfileUpdate_UI_06</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
