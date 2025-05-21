4e73761192ec7aabf06cb808e7b894e219671937
phat.tcu@katalon.com <katalon_g3_dummy_username>
2025-05-21 18:54:27 +0700

    TO commit new changes

---
+++ Test Suites/New folder 1/New Test Suites 05212025-01 (6).ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <name>New Test Suites 05212025-01 (6)</name>
+   <description></description>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ffe23783-6c36-4398-9a2a-49d387109af8</testSuiteGuid>
+</TestSuiteEntity>
